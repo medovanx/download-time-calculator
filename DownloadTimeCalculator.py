7199eb52d5a68954969e4005afc56ceca724c33b
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2023-02-01 19:07:15 +0200

    Update DownloadTimeCalculator.py

--- DownloadTimeCalculator.py
+++ DownloadTimeCalculator.py
@@ -1,11 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QRadioButton, QButtonGroup
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QRadioButton, QButtonGroup, QTabWidget 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
 
-def convert_to_bytes(size, units):
+
+def sizeToBytes(size, units):
     units = units.lower()
-    if units == "gb":
+    if units == "tb":
+        return size * (1024 ** 4)
+    elif units == "gb":
         return size * (1024 ** 3)
     elif units == "mb":
         return size * (1024 ** 2)
@@ -14,7 +17,8 @@ def convert_to_bytes(size, units):
     elif units == "b":
         return size
 
-def convert_speed_to_bytes_per_second(speed, units):
+
+def speedToBytes(speed, units):
     units = units.lower()
     if units == "mbit/s":
         return speed * (1024 ** 2) / 8
@@ -29,116 +33,206 @@ def convert_speed_to_bytes_per_second(speed, units):
     elif units == "b/s":
         return speed
 
-def download_time(size, size_units, speed, speed_units):
-    size_in_bytes = convert_to_bytes(size, size_units)
-    speed_in_bytes_per_second = convert_speed_to_bytes_per_second(speed, speed_units)
-    return size_in_bytes / speed_in_bytes_per_second / 3600 
+
+def DownloadTime(size, sizeUnit, speed, speedUnit):
+    sizeInBytes = sizeToBytes(size, sizeUnit)
+    speedInBytes = speedToBytes(speed, speedUnit)
+    return sizeInBytes / speedInBytes / 3600
+
 
 class DownloadTimeCalculator(QWidget):
-
     def __init__(self):
         super().__init__()
+
         self.setWindowTitle("Download Time Calculator")
-        self.setWindowIcon(QtGui.QIcon(r"M:\Mohamed\Programming\Download Time Calculator\ico.png"))
-        self.setGeometry(100, 100, 400, 100)
-        self.size_label = QLabel("File Size:")
-        self.size_edit = QLineEdit()
+        self.setWindowIcon(QtGui.QIcon(
+            r"M:\Mohamed\Programming\Download Time Calculator\ico.png"))
+        self.setFixedHeight(300)
+        # create label for file size
+        self.sizeLabel = QLabel("File Size")
+        self.sizeLabel.setStyleSheet("QLabel {font-weight: bold; font-size: 15px; font-family: Tahoma;}")
+        self.sizeLabelInput = QLineEdit()
+        self.sizeLabelInput.setValidator(QtGui.QDoubleValidator())
+        # placeholder text
+        self.sizeLabelInput.setPlaceholderText("Enter file size")
+        self.sizeLabelInput.setStyleSheet("QLineEdit {padding: 6px;font-size: 11px;border-width: 1px;border-color: #CCCCCC;background-color: #FFFFFF;border-style: solid;border-radius: 4px;}"
+                                          "QLineEdit:focus {border-color: #1BC466;}")
 
-        #create checkbox for size units
-        self.size_gb = QRadioButton("GB")
-        self.size_mb = QRadioButton("MB")
-        self.size_kb = QRadioButton("KB")
-        self.size_b = QRadioButton("B")
-        self.size_unit_group = QButtonGroup(self)
-        self.size_unit_group.addButton(self.size_gb)
-        self.size_unit_group.addButton(self.size_mb)
-        self.size_unit_group.addButton(self.size_kb)
-        self.size_unit_group.addButton(self.size_b)
-        self.size_unit_group.setExclusive(True)
+        # create Radio buttons for file size units
+        self.sizeTB = QRadioButton("TB")
+        self.sizeGB = QRadioButton("GB")
+        self.sizeMB = QRadioButton("MB")
+        self.sizeKB = QRadioButton("KB")
+        self.sizeB = QRadioButton("B")
 
-        self.speed_label = QLabel("Download Speed:")
-        self.speed_edit = QLineEdit()
-#make separate radio buttons for speed units
-        self.speed_mb = QRadioButton("MB/s")
-        self.speed_kb = QRadioButton("KB/s")
-        self.speed_b = QRadioButton("B/s")
-        self.speed_mbit = QRadioButton("MBit/s")
-        self.speed_kbit = QRadioButton("KBit/s")
-        self.speed_bit = QRadioButton("Bit/s")
-        self.speed_unit_group = QButtonGroup(self)
-        self.speed_unit_group.addButton(self.speed_mb)
-        self.speed_unit_group.addButton(self.speed_kb)
-        self.speed_unit_group.addButton(self.speed_b)
-        self.speed_unit_group.addButton(self.speed_mbit)
-        self.speed_unit_group.addButton(self.speed_kbit)
-        self.speed_unit_group.addButton(self.speed_bit)
-        self.speed_unit_group.setExclusive(True)
+        self.sizeGroup = QButtonGroup(self)
+        self.sizeGroup.addButton(self.sizeTB)
+        self.sizeGroup.addButton(self.sizeGB)
+        self.sizeGroup.addButton(self.sizeMB)
+        self.sizeGroup.addButton(self.sizeKB)
+        self.sizeGroup.addButton(self.sizeB)
+        self.sizeGroup.setExclusive(True)
 
-        self.calculate_button = QPushButton("Calculate")
-        self.calculate_button.clicked.connect(self.calculate)
+        # create label for download speed
+        self.speedLabel = QLabel("Download Speed")
+        self.speedLabelHint = QLabel("Click the speed test button to automatically set the value")
+        self.speedLabel.setStyleSheet("QLabel {font-weight: bold; font-size: 15px; font-family: Tahoma;}")
+        self.speedLabelInput = QLineEdit()
+        self.speedLabelInput.setValidator(QtGui.QDoubleValidator())
+        self.speedLabelInput.setPlaceholderText("Enter download speed")
 
-        self.result_label = QLabel()
+        self.speedLabelInput.setStyleSheet(
+            "QLineEdit {padding: 6px;font-size: 11px;border-width: 1px;border-color: #CCCCCC;background-color: #FFFFFF;border-style: solid;border-radius: 4px;}"
+            "QLineEdit:focus {border-color: #1BC466;}")
 
-        size_layout = QHBoxLayout()
-        size_layout.addWidget(self.size_label)
-        size_layout.addWidget(self.size_edit)
-        size_units_layout = QHBoxLayout()
-        size_units_layout.addWidget(self.size_gb)
-        size_units_layout.addWidget(self.size_mb)
-        size_units_layout.addWidget(self.size_kb)
-        size_units_layout.addWidget(self.size_b)
+        # create Radio buttons for download speed units
+        self.speedMB = QRadioButton("MB/s")
+        self.speedKB = QRadioButton("KB/s")
+        self.speedB = QRadioButton("B/s")
+        self.speedMbit = QRadioButton("MBit/s")
+        self.speedKbit = QRadioButton("KBit/s")
+        self.speedBit = QRadioButton("Bit/s")
 
-        speed_layout = QHBoxLayout()
-        speed_layout.addWidget(self.speed_label)
-        speed_layout.addWidget(self.speed_edit)
+        self.speedGroup = QButtonGroup(self)
+        self.speedGroup.addButton(self.speedMB)
+        self.speedGroup.addButton(self.speedKB)
+        self.speedGroup.addButton(self.speedB)
+        self.speedGroup.addButton(self.speedMbit)
+        self.speedGroup.addButton(self.speedKbit)
+        self.speedGroup.addButton(self.speedBit)
+        self.speedGroup.setExclusive(True)
 
-        speed_units_layout = QHBoxLayout()
-        speed_units_layout.addWidget(self.speed_mb)
-        speed_units_layout.addWidget(self.speed_kb)
-        speed_units_layout.addWidget(self.speed_b)
-        speed_units_layout.addWidget(self.speed_mbit)
-        speed_units_layout.addWidget(self.speed_kbit)
-        speed_units_layout.addWidget(self.speed_bit)
+        # create button to calculate download time
+        self.CalculateButton = QPushButton("Calculate")
+        self.CalculateButton.setStyleSheet("""
+        QPushButton {
+            color: #FFFFFF;
+            font-family: Verdana;
+            font-size: 13px;
+            font-weight: bold;
+            padding: 6px;
+            background-color: #1BC466;
+            border-radius: 10px;
+            }
+        QPushButton:hover {
+            background:#0B5724;
+            }
+         """)
+        self.CalculateButton.clicked.connect(self.calculate)
 
-        author = QLabel('Created by: Mohamed Darwesh (<a href="https://github.com/medovanx">@medovanx</a>)', self)
-        author.move(10, 240)
-        author.setOpenExternalLinks(True)
-        author.setStyleSheet("""font-weight: bold; font-size: 12px;""")
+        # create button to test download speed
+        self.testSpeedButton = QPushButton("Speed Test (Wait 10s)")
+        self.testSpeedButton.clicked.connect(self.SpeedTest)
+        self.testSpeedButton.setStyleSheet("""
+        QPushButton {
+            color: #FFFFFF;
+            font-family: Verdana;
+            font-size: 13px;
+            font-weight: bold;
+            padding: 6px;
+            background-color: #3D94F6;
+            border-radius: 10px;
+            }
+        QPushButton:hover {
+            border: solid #337FED 1px;
+            background: #1E62D0;
+            }
+         """)
 
 
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(size_layout)
-        main_layout.addLayout(size_units_layout)
-        main_layout.addLayout(speed_layout)
-        main_layout.addLayout(speed_units_layout)
-        main_layout.addWidget(self.calculate_button)
-        main_layout.addWidget(self.result_label)
-        main_layout.addWidget(author)
+        sizeUnitLayout = QHBoxLayout()
+        sizeUnitLayout.addStretch(1)
+        sizeUnitLayout.addWidget(self.sizeTB)
+        sizeUnitLayout.addWidget(self.sizeGB)
+        sizeUnitLayout.addWidget(self.sizeMB)
+        sizeUnitLayout.addWidget(self.sizeKB)
+        sizeUnitLayout.addWidget(self.sizeB)
+        sizeUnitLayout.addStretch(1)
 
+        speedUnitLayout = QHBoxLayout()
+        speedUnitLayout.addStretch(1)
+        speedUnitLayout.addWidget(self.speedMB)
+        speedUnitLayout.addWidget(self.speedKB)
+        speedUnitLayout.addWidget(self.speedB)
+        speedUnitLayout.addWidget(self.speedMbit)
+        speedUnitLayout.addWidget(self.speedKbit)
+        speedUnitLayout.addWidget(self.speedBit)
+        speedUnitLayout.addStretch(1)
 
-        self.setLayout(main_layout)
+        self.Result = QLabel()
+        self.Result.setAlignment(Qt.AlignCenter)
+        self.Result.setStyleSheet("QLabel {font-weight: bold; font-size: 13px; font-family: Tahoma;}")
+
+        self.Developer = QLabel(
+            '© Mohamed Darwesh (<a href="https://github.com/medovanx">@medovanx</a>)')
+        self.Developer.setOpenExternalLinks(True)
+        self.Developer.setStyleSheet("""font-weight: bold; font-size: 10px;""")
+        self.Developer.setAlignment(Qt.AlignLeft)
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.sizeLabel)
+        mainLayout.addWidget(self.sizeLabelInput)
+        mainLayout.addLayout(sizeUnitLayout)
+        mainLayout.addWidget(self.speedLabel)
+        mainLayout.addWidget(self.speedLabelHint)
+        mainLayout.addWidget(self.speedLabelInput)
+        mainLayout.addLayout(speedUnitLayout)
+        mainLayout.addWidget(self.CalculateButton)
+        mainLayout.addWidget(self.testSpeedButton)
+        mainLayout.addWidget(self.Result)
+        mainLayout.addWidget(self.Developer)
+
+        self.setLayout(mainLayout)
+
+    def SpeedTest(self):
+        import speedtest
+        st = speedtest.Speedtest()
+        st.get_best_server()
+        try:
+            self.speedLabelInput.setText(str(st.download() / 10**6))
+            self.speedMbit.setChecked(True)
+        except:
+            self.Result.setText(f"Error occured while testing speed")
+            return
 
     def calculate(self):
         try:
-            size = float(self.size_edit.text())
-            size_units = self.size_gb.text() if self.size_gb.isChecked() else self.size_mb.text() if self.size_mb.isChecked() else self.size_kb.text() if self.size_kb.isChecked() else self.size_b.text()
-            speed = float(self.speed_edit.text())
-            speed_units = self.speed_mb.text() if self.speed_mb.isChecked() else self.speed_kb.text() if self.speed_kb.isChecked() else self.speed_b.text() if self.speed_b.isChecked() else self.speed_mbit.text() if self.speed_mbit.isChecked() else self.speed_kbit.text() if self.speed_kbit.isChecked() else self.speed_bit.text()
+            size = float(self.sizeLabelInput.text())
+            sizeUnit = self.sizeTB.text() if self.sizeTB.isChecked() else self.sizeGB.text() if self.sizeGB.isChecked() else self.sizeMB.text(
+            ) if self.sizeMB.isChecked() else self.sizeKB.text() if self.sizeKB.isChecked() else self.sizeB.text()
+            speed = float(self.speedLabelInput.text())
+            speedUnit = self.speedMB.text() if self.speedMB.isChecked() else self.speedKB.text() if self.speedKB.isChecked() else self.speedB.text() if self.speedB.isChecked(
+            ) else self.speedMbit.text() if self.speedMbit.isChecked() else self.speedKbit.text() if self.speedKbit.isChecked() else self.speedBit.text()
         except ValueError:
-            self.result_label.setText("<font color='red'>Please enter a valid number.</font>")
-            self.result_label.setAlignment(Qt.AlignCenter)
+            self.Result.setText(
+                "<font color='red'>Please enter a valid value.</font>")
             return
-        time = download_time(size, size_units, speed, speed_units)
-        #write time in days, hours, minutes, and seconds
-        days = int(time // 24)
-        hours = int(time % 24)
-        minutes = int((time * 60) % 60)
-        seconds = int((time * 3600) % 60)
-        milliseconds = int((time * 3600000) % 1000)
-        self.result_label.setText(f"Download Time: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds and {milliseconds} milliseconds")
-        self.result_label.setAlignment(Qt.AlignCenter)
+
+        time = DownloadTime(size, sizeUnit, speed, speedUnit)
+        months = int(time // 30)
+        days = int(time % 30)
+        hours = int(time * 24 % 24)
+        minutes = int(time * 24 * 60 % 60)
+        seconds = int(time * 24 * 60 * 60 % 60)
+        if months == 0 and days == 0 and hours == 0 and minutes == 0 and seconds == 0:
+            self.Result.setText(f"Download Time: less than a second")
+        elif months == 0:
+            self.Result.setText(
+                f"Download Time: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
+        elif months == 0 and days == 0:
+            self.Result.setText(
+                f"Download Time: {hours} hours, {minutes} minutes, {seconds} seconds")
+        elif months == 0 and days == 0 and hours == 0:
+            self.Result.setText(
+                f"Download Time: {minutes} minutes, {seconds} seconds")
+        elif months == 0 and days == 0 and hours == 0 and minutes == 0:
+            self.Result.setText(f"Download Time: {seconds} seconds")
+        else:
+            self.Result.setText(
+                f"Download Time: {months} months, {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
+
 
 app = QApplication(sys.argv)
-calculator = DownloadTimeCalculator()
-calculator.show()
+window = DownloadTimeCalculator()
+window.show()
 sys.exit(app.exec_())
